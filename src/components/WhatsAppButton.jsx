10e7ee2ffc95ee3e96f2c109eb8c001ac30f8549
diatsilva007 <diatsilva007@gmail.com>
2025-05-20 00:25:27 -0300

    Formata o código do componente WhatsAppButton para manter consistência no estilo de importação

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { FaWhatsapp } from 'react-icons/fa';
-import '../App.css';
+import React from "react";
+import { FaWhatsapp } from "react-icons/fa";
+import "../App.css";
 
 const WhatsAppButton = () => (
   <a
